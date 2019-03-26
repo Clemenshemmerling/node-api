088e18c58f880b7e0845625c67ac05ad36209614
Clemens Hemmerling <chemmerling@grupomacro.com>
2019-03-25 20:18:13 -0600

    Agregamos socket io y axios para lograr obnbeter la llave

--- app/app.js
+++ app/app.js
@@ -6,11 +6,37 @@ const express = require('express'),
   mongoose = require('mongoose'),
   xml = require('./api/models/firmaModel'),
   receptor = require('./api/models/receptorModel'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  axios = require('axios'),
+  convert = require('xml-js');
 
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mongo/Facturadb', {useNewUrlParser: true });
+
+const postedData =
+  `<?xml version='1.0' encoding='UTF-8'?>
+  <SolicitaTokenRequest>
+    <usuario>47250763</usuario>
+    <apikey>2CjGSRYDfrkXOcW2xQbOEVV</apikey>
+  </SolicitaTokenRequest>`;
+let key = '';
+
+axios.post('https://dev.api.ifacere-fel.com/fel-dte-services/api/solicitarToken', postedData, {
+  headers: {
+    'content-type': 'application/xml'
+  }
+})
+  .then(res => {
+    key = convert.xml2js(res.data, {compact: true, spaces: 2});
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+io.sockets.on('connect', socket => {
+  io.sockets.emit('key', key);
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
